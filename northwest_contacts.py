984e7f4f5a95a2df62aae8bff3f83002a841d50f
Pip Clubbs <70511582+pipclubbs@users.noreply.github.com>
2023-10-06 22:12:09 +0100

    Update northwest_contacts.py - tidy and comment

--- northwest_contacts.py
+++ northwest_contacts.py
@@ -1,12 +1,17 @@
+"""module containing the class that scrapes websites for climbing venue data (North West)"""
 import datetime
 import db_conn
 from class_scrapers import ClassScraper
 
 
 class NorthWestContacts(ClassScraper):
+    """class containing the web scraping information for climbing clubs (North West)"""
     def __init__(self):
         super().__init__()
 
+
+    """method that defines the sites to scrape, then runs through each in turn and sorts the results
+    into dictionaries ready for storing in the database"""
     def assign_values(self):
         created = datetime.datetime.now()
         output = []
@@ -30,13 +35,15 @@ class NorthWestContacts(ClassScraper):
             if c["name"] == "Eden Rock, Carlisle":
                 contactUrl = c["contactUrl"]
                 area = c["area"]
-                soup = self.get_html(contactUrl)
+                soup = self.get_html(contactUrl) # get the html via the parent class
 
                 if soup:
+                    # search for the tags in the html
                     span_tags = self.search_tags('span', soup)
                     span_tags = [i for n, i in enumerate(
                         span_tags) if i not in span_tags[:n]]
 
+                    # allocate the results to a dictionary for storing in the database
                     centre_details = [
                         {
                             "area": area,
@@ -53,6 +60,7 @@ class NorthWestContacts(ClassScraper):
                             "created": created
                         }
                     ]
+                    # append the dictionary to a list
                     for i in centre_details:
                         scraped_centres.append(i)
 
@@ -89,5 +97,7 @@ class NorthWestContacts(ClassScraper):
                 else:
                     pass
 
+        """send the compiled centre list to the database module, and
+        append the result to the output list"""
         output.append(db_conn.DatabaseConnection(scraped_centres))
         return output
