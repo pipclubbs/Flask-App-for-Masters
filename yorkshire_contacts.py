a981606d14c88dd5f8f48e85389a9349d99ff869
Pip Clubbs <70511582+pipclubbs@users.noreply.github.com>
2023-10-06 22:34:27 +0100

    Update yorkshire_contacts.py - tidy and comment (and remove repeated functions)

--- yorkshire_contacts.py
+++ yorkshire_contacts.py
@@ -1,3 +1,4 @@
+"""module containing the class that scrapes websites for climbing centre data (Yorkshire)"""
 import datetime
 import re
 import db_conn
@@ -5,9 +6,13 @@ from class_scrapers import ClassScraper
 
 
 class YorkshireContacts(ClassScraper):
+    """class containing the web scraping information for climbing centres (Yorkshire)"""
     def __init__(self):
         super().__init__()
 
+
+    """method that defines the sites to scrape, then runs through each in turn and sorts the results
+    into dictionaries ready for storing in the database"""
     def assign_values(self):
         created = datetime.datetime.now()
         output = []
@@ -40,9 +45,10 @@ class YorkshireContacts(ClassScraper):
             if c["name"] == "The Foundry, Sheffield":
                 contactUrl = c["contactUrl"]
                 area = c["area"]
-                soup = self.get_html(contactUrl)
+                soup = self.get_html(contactUrl) # get html via the parent class
 
                 if soup:
+                    # search for the tags in the html
                     div_tags = self.search_tags('div', soup)
                     div_tags = self.remove_blanks(div_tags)
                     div_tags = self.tidy_tags(div_tags)
@@ -56,6 +62,7 @@ class YorkshireContacts(ClassScraper):
                     a_tags = self.remove_blanks(a_tags)
                     a_tags = self.tidy_tags(a_tags)
 
+                    # allocate the results to a dictionary for storing in the database
                     centre_details = [
                         {
                             "area": area,
@@ -72,6 +79,7 @@ class YorkshireContacts(ClassScraper):
                             "created": created
                         }
                     ]
+                    
                     for i in centre_details:
                         scraped_centres.append(i)
 
@@ -182,37 +190,13 @@ class YorkshireContacts(ClassScraper):
                 else:
                     pass
 
+        """send the compiled centre list to the database module, and 
+        append the result to the output list"""
         output.append(db_conn.DatabaseConnection(scraped_centres))
         return output
 
-    def extract_contacts(self, string):
-        # Define regular expressions for email address and phone number
-        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-        phone_pattern = r'(\b\d{4} \d{3} \d{4}\b)|(\b\d{4} \d{7}\b)|(\b\d{5} \d{3} \d{3}\b)'
 
-        # Find email addresses and phone numbers using re.findall
-        email_addresses = re.findall(email_pattern, string)
-        phone_numbers = re.findall(phone_pattern, string)
-
-        # Extracted email addresses and phone numbers
-        return [email_addresses, phone_numbers]
-
-    def split_string(self, string):
-        # Split the string into a list of strings, using the regular expression as the delimiter.
-        string_list = re.sub(
-            r'([a-z])([A-Z])', r'\1 \2', string)
-        string_list = re.sub(r'(:)([A-Z])', r'\1 \2', string_list)
-        string_list = re.sub(r'([a-z])([0-9])', r'\1 \2', string_list)
-        string_list = re.sub(r'(,)([A-Z])', r' \2', string_list)
-        words = string_list.split()
-        word_list = ' '.join(words)
-
-        word_list = re.split("\s+", word_list)
-        # Remove any empty strings from the list.
-        # string_list = [word for word in string_list if word]
-
-        return word_list
-
+    """method to tidy the tags"""
     def tidy_tags(self, list):
         new_list = []
         for i in list:
