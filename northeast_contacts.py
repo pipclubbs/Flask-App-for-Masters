6a2d2d05ee410de18dda1ac11b3d10887d35c905
Pip Clubbs <70511582+pipclubbs@users.noreply.github.com>
2023-10-11 10:30:11 +0100

    Update northeast_contacts.py - adding error handling

--- northeast_contacts.py
+++ northeast_contacts.py
@@ -45,139 +45,155 @@ class NorthEastContacts(ClassScraper):
         ]
 
         for c in centres:
-            if c["name"] == "Climb Valley, Newcastle":
-                contactUrl = c["contactUrl"]
-                area = c["area"]
-                soup = self.get_html(contactUrl) # get the html via the parent class method
+            try:
+                if c["name"] == "Climb Valley, Newcastle":
+                    contactUrl = c["contactUrl"]
+                    area = c["area"]
+                    soup = self.get_html(contactUrl) # get the html via the parent class method
+    
+                    if soup:
+                        # search for the tags in the html
+                        p_tags = self.search_tags_alternative('p', soup)
+                        p_tags_address_list = self.split_string(p_tags[3])
+                        p_tags_contacts_list = self.extract_contacts(p_tags[24])
+    
+                        # allocate the results to a dictionary for storing in the database
+                        centre_details = [
+                            {
+                                "area": area,
+                                "name": c["name"],
+                                "homeUrl": c["homeUrl"],
+                                "contactUrl": contactUrl,
+                                "classUrl": '',
+                                "street": f'{p_tags_address_list[5]} {p_tags_address_list[6]}',
+                                "street_area": '',
+                                "city": f'{p_tags_address_list[7]} {p_tags_address_list[8]} {p_tags_address_list[9]}',
+                                "postcode": f'{p_tags_address_list[10]} {p_tags_address_list[11]}',
+                                "email": p_tags_contacts_list[0][0],
+                                "phone": p_tags_contacts_list[1][0][0],
+                                "created": created
+                            }
+                        ]
+                        # add each dictionary entry to a list
+                        for i in centre_details:
+                            scraped_centres.append(i)
+    
+                    else:
+                        pass
 
-                if soup:
-                    # search for the tags in the html
-                    p_tags = self.search_tags_alternative('p', soup)
-                    p_tags_address_list = self.split_string(p_tags[3])
-                    p_tags_contacts_list = self.extract_contacts(p_tags[24])
+            except:
+                pass
 
-                    # allocate the results to a dictionary for storing in the database
-                    centre_details = [
-                        {
-                            "area": area,
-                            "name": c["name"],
-                            "homeUrl": c["homeUrl"],
-                            "contactUrl": contactUrl,
-                            "classUrl": '',
-                            "street": f'{p_tags_address_list[5]} {p_tags_address_list[6]}',
-                            "street_area": '',
-                            "city": f'{p_tags_address_list[7]} {p_tags_address_list[8]} {p_tags_address_list[9]}',
-                            "postcode": f'{p_tags_address_list[10]} {p_tags_address_list[11]}',
-                            "email": p_tags_contacts_list[0][0],
-                            "phone": p_tags_contacts_list[1][0][0],
-                            "created": created
-                        }
-                    ]
-                    # add each dictionary entry to a list
-                    for i in centre_details:
-                        scraped_centres.append(i)
+            try:
+                if c["name"] == "Durham Climbing Centre, Durham":
+                    contactUrl = c["contactUrl"]
+                    area = c["area"]
+                    soup = self.get_html(contactUrl)
+    
+                    if soup:
+                        li_tags = self.search_tags_alternative('li', soup)
+                        li_tags_address_list = self.split_string(li_tags[0])
+                        li_tags_phone = self.extract_contacts(li_tags[1])
+    
+                        centre_details = [
+                            {
+                                "area": area,
+                                "name": c["name"],
+                                "homeUrl": c["homeUrl"],
+                                "contactUrl": contactUrl,
+                                "classUrl": '',
+                                "street": f'{li_tags_address_list[0]} {li_tags_address_list[1]} {li_tags_address_list[2]}',
+                                "street_area": f'{li_tags_address_list[3]} {li_tags_address_list[4]} {li_tags_address_list[5]}',
+                                "city": li_tags_address_list[6],
+                                "postcode": f'{li_tags_address_list[9]} {li_tags_address_list[10]}',
+                                "email": "Please visit website directly for email address",
+                                "phone": li_tags_phone[1][0][0],
+                                "created": created
+                            }
+                        ]
+                        for i in centre_details:
+                            scraped_centres.append(i)
+    
+                    else:
+                        pass
 
-                else:
-                    pass
+            except:
+                pass
 
-            if c["name"] == "Durham Climbing Centre, Durham":
-                contactUrl = c["contactUrl"]
-                area = c["area"]
-                soup = self.get_html(contactUrl)
+            try:
+                if c["name"] == "Sunderland Wall, Sunderland":
+                    homeUrl = c["homeUrl"]
+                    area = c["area"]
+                    soup = self.get_html("https://sunderlandwall.com/home/")
+    
+                    if soup:
+                        p_tags_address = []
+                        p_tags = self.search_tags_alternative('p', soup)
+                        p_tags_contacts = self.extract_contacts(p_tags[2])
+                        p_tags_address = re.sub(r'(,)([A-Z])', r' \2', p_tags[3])
+                        p_tags_address = re.split("\s+", p_tags_address)
+    
+                        centre_details = [
+                            {
+                                "area": area,
+                                "name": c["name"],
+                                "homeUrl": homeUrl,
+                                "contactUrl": c["contactUrl"],
+                                "classUrl": '',
+                                "street": f'{p_tags_address[2]} {p_tags_address[3]}',
+                                "street_area": f'{p_tags_address[4]} {p_tags_address[5]}',
+                                "city": p_tags_address[0],
+                                "postcode": f'{p_tags_address[6]} {p_tags_address[7]}',
+                                "email": p_tags_contacts[0][0],
+                                "phone": p_tags_contacts[1][0][1],
+                                "created": created
+                            }
+                        ]
+                        for i in centre_details:
+                            scraped_centres.append(i)
+    
+                    else:
+                        pass
 
-                if soup:
-                    li_tags = self.search_tags_alternative('li', soup)
-                    li_tags_address_list = self.split_string(li_tags[0])
-                    li_tags_phone = self.extract_contacts(li_tags[1])
+            except:
+                pass
 
-                    centre_details = [
-                        {
-                            "area": area,
-                            "name": c["name"],
-                            "homeUrl": c["homeUrl"],
-                            "contactUrl": contactUrl,
-                            "classUrl": '',
-                            "street": f'{li_tags_address_list[0]} {li_tags_address_list[1]} {li_tags_address_list[2]}',
-                            "street_area": f'{li_tags_address_list[3]} {li_tags_address_list[4]} {li_tags_address_list[5]}',
-                            "city": li_tags_address_list[6],
-                            "postcode": f'{li_tags_address_list[9]} {li_tags_address_list[10]}',
-                            "email": "Please visit website directly for email address",
-                            "phone": li_tags_phone[1][0][0],
-                            "created": created
-                        }
-                    ]
-                    for i in centre_details:
-                        scraped_centres.append(i)
+            try:
+                if c["name"] == "Newcastle Climbing Centre, Byker":
+                    contactUrl = c["contactUrl"]
+                    area = c["area"]
+                    soup = self.get_html(contactUrl)
+    
+                    if soup:
+                        p_tags = self.search_tags_alternative(
+                            'p', soup)
+                        p_tags_address_list = self.split_string(p_tags[1])
+                        p_tags_contact_list = self.extract_contacts(p_tags[2])
+    
+                        centre_details = [
+                            {
+                                "area": area,
+                                "name": c["name"],
+                                "homeUrl": c["homeUrl"],
+                                "contactUrl": contactUrl,
+                                "classUrl": '',
+                                "street": f'{p_tags_address_list[3]} {p_tags_address_list[4]} {p_tags_address_list[5]}',
+                                "street_area": f'{p_tags_address_list[6]} {p_tags_address_list[7]} {p_tags_address_list[8]}',
+                                "city": f'{p_tags_address_list[9]} {p_tags_address_list[10]} {p_tags_address_list[11]}',
+                                "postcode": f'{p_tags_address_list[12]} {p_tags_address_list[13]}',
+                                "email": p_tags_contact_list[0][0],
+                                "phone": p_tags_contact_list[1][0][0],
+                                "created": created
+                            }
+                        ]
+                        for i in centre_details:
+                            scraped_centres.append(i)
+    
+                    else:
+                        pass
 
-                else:
-                    pass
-
-            if c["name"] == "Sunderland Wall, Sunderland":
-                homeUrl = c["homeUrl"]
-                area = c["area"]
-                soup = self.get_html("https://sunderlandwall.com/home/")
-
-                if soup:
-                    p_tags_address = []
-                    p_tags = self.search_tags_alternative('p', soup)
-                    p_tags_contacts = self.extract_contacts(p_tags[2])
-                    p_tags_address = re.sub(r'(,)([A-Z])', r' \2', p_tags[3])
-                    p_tags_address = re.split("\s+", p_tags_address)
-
-                    centre_details = [
-                        {
-                            "area": area,
-                            "name": c["name"],
-                            "homeUrl": homeUrl,
-                            "contactUrl": c["contactUrl"],
-                            "classUrl": '',
-                            "street": f'{p_tags_address[2]} {p_tags_address[3]}',
-                            "street_area": f'{p_tags_address[4]} {p_tags_address[5]}',
-                            "city": p_tags_address[0],
-                            "postcode": f'{p_tags_address[6]} {p_tags_address[7]}',
-                            "email": p_tags_contacts[0][0],
-                            "phone": p_tags_contacts[1][0][1],
-                            "created": created
-                        }
-                    ]
-                    for i in centre_details:
-                        scraped_centres.append(i)
-
-                else:
-                    pass
-
-            if c["name"] == "Newcastle Climbing Centre, Byker":
-                contactUrl = c["contactUrl"]
-                area = c["area"]
-                soup = self.get_html(contactUrl)
-
-                if soup:
-                    p_tags = self.search_tags_alternative(
-                        'p', soup)
-                    p_tags_address_list = self.split_string(p_tags[1])
-                    p_tags_contact_list = self.extract_contacts(p_tags[2])
-
-                    centre_details = [
-                        {
-                            "area": area,
-                            "name": c["name"],
-                            "homeUrl": c["homeUrl"],
-                            "contactUrl": contactUrl,
-                            "classUrl": '',
-                            "street": f'{p_tags_address_list[3]} {p_tags_address_list[4]} {p_tags_address_list[5]}',
-                            "street_area": f'{p_tags_address_list[6]} {p_tags_address_list[7]} {p_tags_address_list[8]}',
-                            "city": f'{p_tags_address_list[9]} {p_tags_address_list[10]} {p_tags_address_list[11]}',
-                            "postcode": f'{p_tags_address_list[12]} {p_tags_address_list[13]}',
-                            "email": p_tags_contact_list[0][0],
-                            "phone": p_tags_contact_list[1][0][0],
-                            "created": created
-                        }
-                    ]
-                    for i in centre_details:
-                        scraped_centres.append(i)
-
-                else:
-                    pass
+            except:
+                pass
 
         """send the compiled centre list to the database module, 
         and append the result to the output list"""
