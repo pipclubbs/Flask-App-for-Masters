7ab25a8d859f78e09ce4881db19bf35bfd8bf82f
Pip Clubbs <70511582+pipclubbs@users.noreply.github.com>
2023-10-11 10:23:50 +0100

    Update northwest_contacts.py - adding error handling

--- northwest_contacts.py
+++ northwest_contacts.py
@@ -32,71 +32,79 @@ class NorthWestContacts(ClassScraper):
         ]
 
         for c in centres:
-            if c["name"] == "Eden Rock, Carlisle":
-                contactUrl = c["contactUrl"]
-                area = c["area"]
-                soup = self.get_html(contactUrl) # get the html via the parent class
+            try:
+                if c["name"] == "Eden Rock, Carlisle":
+                    contactUrl = c["contactUrl"]
+                    area = c["area"]
+                    soup = self.get_html(contactUrl) # get the html via the parent class
+    
+                    if soup:
+                        # search for the tags in the html
+                        span_tags = self.search_tags('span', soup)
+                        span_tags = [i for n, i in enumerate(
+                            span_tags) if i not in span_tags[:n]]
+    
+                        # allocate the results to a dictionary for storing in the database
+                        centre_details = [
+                            {
+                                "area": area,
+                                "name": c["name"],
+                                "homeUrl": c["homeUrl"],
+                                "contactUrl": contactUrl,
+                                "classUrl": '',
+                                "street": span_tags[4],
+                                "street_area": span_tags[5],
+                                "city": span_tags[6],
+                                "postcode": span_tags[7],
+                                "email": span_tags[10],
+                                "phone": span_tags[12],
+                                "created": created
+                            }
+                        ]
+                        # append the dictionary to a list
+                        for i in centre_details:
+                            scraped_centres.append(i)
+    
+                    else:
+                        pass
 
-                if soup:
-                    # search for the tags in the html
-                    span_tags = self.search_tags('span', soup)
-                    span_tags = [i for n, i in enumerate(
-                        span_tags) if i not in span_tags[:n]]
+            except:
+                pass
 
-                    # allocate the results to a dictionary for storing in the database
-                    centre_details = [
-                        {
-                            "area": area,
-                            "name": c["name"],
-                            "homeUrl": c["homeUrl"],
-                            "contactUrl": contactUrl,
-                            "classUrl": '',
-                            "street": span_tags[4],
-                            "street_area": span_tags[5],
-                            "city": span_tags[6],
-                            "postcode": span_tags[7],
-                            "email": span_tags[10],
-                            "phone": span_tags[12],
-                            "created": created
-                        }
-                    ]
-                    # append the dictionary to a list
-                    for i in centre_details:
-                        scraped_centres.append(i)
+            try:
+                if c["name"] == "Blochaus Climbing, Manchester":
+                    contactUrl = c["contactUrl"]
+                    area = c["area"]
+                    soup = self.get_html(contactUrl)
+    
+                    if soup:
+                        p_tags = self.search_tags('p', soup)
+    
+                        centre_details = [
+                            {
+                                "area": area,
+                                "name": c["name"],
+                                "homeUrl": c["homeUrl"],
+                                "contactUrl": contactUrl,
+                                "classUrl": '',
+                                "street": p_tags[13],
+                                "street_area": p_tags[14],
+                                "city": p_tags[15],
+                                "postcode": p_tags[16],
+                                "email": p_tags[24],
+                                "phone": p_tags[23],
+                                "created": created
+                            }
+                        ]
+                        for i in centre_details:
+                            scraped_centres.append(i)
+    
+                    else:
+                        pass
 
-                else:
-                    pass
-
-            if c["name"] == "Blochaus Climbing, Manchester":
-                contactUrl = c["contactUrl"]
-                area = c["area"]
-                soup = self.get_html(contactUrl)
-
-                if soup:
-                    p_tags = self.search_tags('p', soup)
-
-                    centre_details = [
-                        {
-                            "area": area,
-                            "name": c["name"],
-                            "homeUrl": c["homeUrl"],
-                            "contactUrl": contactUrl,
-                            "classUrl": '',
-                            "street": p_tags[13],
-                            "street_area": p_tags[14],
-                            "city": p_tags[15],
-                            "postcode": p_tags[16],
-                            "email": p_tags[24],
-                            "phone": p_tags[23],
-                            "created": created
-                        }
-                    ]
-                    for i in centre_details:
-                        scraped_centres.append(i)
-
-                else:
-                    pass
-
+            except:
+                pass
+                
         """send the compiled centre list to the database module, and
         append the result to the output list"""
         output.append(db_conn.DatabaseConnection(scraped_centres))
