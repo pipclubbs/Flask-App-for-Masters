399af4e657c8ef5ab8124ac0a5cd26ed8750234a
Pip Clubbs <70511582+pipclubbs@users.noreply.github.com>
2023-10-11 10:34:11 +0100

    Update midlands_contacts.py - adding error handling

--- midlands_contacts.py
+++ midlands_contacts.py
@@ -26,41 +26,45 @@ class MidlandsContacts(ClassScraper):
         ]
 
         for c in centres:
-            if c["name"] == "YMCA Lincolnshire, Lincoln":
-                contactUrl = c["contactUrl"]
-                area = c["area"]
-                soup = self.get_html(contactUrl) # get the html via the parent class method
+            try:
+                if c["name"] == "YMCA Lincolnshire, Lincoln":
+                    contactUrl = c["contactUrl"]
+                    area = c["area"]
+                    soup = self.get_html(contactUrl) # get the html via the parent class method
+    
+                    if soup:
+                        # search for the tags in the html
+                        p_tags = self.search_tags_alternative('p', soup)
+                        p_tags_address_list = self.split_string(p_tags[15])
+                        p_tags_phone = self.extract_contacts(p_tags[12])
+                        p_tags_email = self.extract_contacts(p_tags[13])
+    
+                        # allocate the results to a dictionary for storing in the database
+                        centre_details = [
+                            {
+                                "area": area,
+                                "name": c["name"],
+                                "homeUrl": c["homeUrl"],
+                                "contactUrl": contactUrl,
+                                "classUrl": '',
+                                "street": f'{p_tags_address_list[0]} {p_tags_address_list[1]}',
+                                "street_area": f'{p_tags_address_list[2]} {p_tags_address_list[3]}',
+                                "city": p_tags_address_list[4],
+                                "postcode": f'{p_tags_address_list[5]} {p_tags_address_list[6]}',
+                                "email": p_tags_email[0][0],
+                                "phone": p_tags_phone[1][0][2],
+                                "created": created
+                            }
+                        ]
+                        # add each dictionary entry to a list
+                        for i in centre_details:
+                            scraped_centres.append(i)
+    
+                    else:
+                        pass
 
-                if soup:
-                    # search for the tags in the html
-                    p_tags = self.search_tags_alternative('p', soup)
-                    p_tags_address_list = self.split_string(p_tags[15])
-                    p_tags_phone = self.extract_contacts(p_tags[12])
-                    p_tags_email = self.extract_contacts(p_tags[13])
-
-                    # allocate the results to a dictionary for storing in the database
-                    centre_details = [
-                        {
-                            "area": area,
-                            "name": c["name"],
-                            "homeUrl": c["homeUrl"],
-                            "contactUrl": contactUrl,
-                            "classUrl": '',
-                            "street": f'{p_tags_address_list[0]} {p_tags_address_list[1]}',
-                            "street_area": f'{p_tags_address_list[2]} {p_tags_address_list[3]}',
-                            "city": p_tags_address_list[4],
-                            "postcode": f'{p_tags_address_list[5]} {p_tags_address_list[6]}',
-                            "email": p_tags_email[0][0],
-                            "phone": p_tags_phone[1][0][2],
-                            "created": created
-                        }
-                    ]
-                    # add each dictionary entry to a list
-                    for i in centre_details:
-                        scraped_centres.append(i)
-
-                else:
-                    pass
+            except:
+                pass
 
         """send the compiled scraped centre list to the database module, 
         and append the result to the output list"""
